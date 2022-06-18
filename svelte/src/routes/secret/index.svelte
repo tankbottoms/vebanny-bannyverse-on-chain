f5944bc6141933508f9ccb51063dcd5eedda2bc6
Evie <evita@stenqvist.co>
2022-06-18 18:20:05 +0200

    Add non-standard characters to tool

--- svelte/src/routes/secret/index.svelte
+++ svelte/src/routes/secret/index.svelte
@@ -2,7 +2,14 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
-	const indices = Array.from(Array(60).keys()).map((i) => i + 1);
+	const standard = Array.from(Array(60).keys()).map((i) => i + 1);
+	let current = 60;
+	const nonstandard = Array.from(Array(16).keys()).map((i) => {
+		current++;
+		return current;
+	});
+
+	// Array from 61 to 76
 
 	async function getCharacter({ bannyIndex }: { bannyIndex: number }) {
 		let index: number | string = bannyIndex;
@@ -30,7 +37,8 @@
 	});
 </script>
 
-{#each indices as banny}
+<h1>Standard Bannies</h1>
+{#each standard as banny}
 	{#await getCharacter({ bannyIndex: banny })}
 		<p>...</p>
 	{:then data}
@@ -52,3 +60,32 @@
 		</svg>
 	{/await}
 {/each}
+<h1>Nonstandard Bannies</h1>
+{#each nonstandard as banny}
+	{#await getCharacter({ bannyIndex: banny })}
+		<p>...</p>
+	{:then data}
+		<svg
+			id="token"
+			width="300"
+			height="300"
+			viewBox="0 0 1080 1080"
+			fill="none"
+			on:click={() => {
+				goto(`secret/${banny}`);
+			}}
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<!-- NOTE: This is where we're adding Banny -->
+			<g id="bannyPlaceholder">
+				{@html data.svgImageString}
+			</g>
+		</svg>
+	{/await}
+{/each}
+
+<style>
+	h1 {
+		font-family: monospace;
+	}
+</style>
