73b7b43c368db62ec273c9681ed3706f49e67c86
Evie <evita@stenqvist.co>
2022-06-21 18:58:35 +0200

    Tweak - add loading indicator (instead of showing undefined on jbxRange)

--- svelte/src/routes/backgrounds.svelte
+++ svelte/src/routes/backgrounds.svelte
@@ -3,7 +3,7 @@
 	import { getLayeredSvgFromBannyIndex } from '$utils/layering';
 	import { onMount } from 'svelte';
 
-    let jbxRange: string;
+	let jbxRange: string;
 
 	const lockPeriods = ['1 WEEK', '1 MONTH', '3 MONTHS', '6 MONTHS', '1 YEAR', '4 YEARS'];
 
@@ -12,29 +12,34 @@
 		for (let i = 1; i <= lockPeriod + 1; i++) {
 			options.push(`planet${i}`);
 		}
-        for (let i = 1; i <= 4; i++) {
-            options.push(`star${i}`);
-            options.push(`dot${i}`);
-        }
+		for (let i = 1; i <= 4; i++) {
+			options.push(`star${i}`);
+			options.push(`dot${i}`);
+		}
 		return options;
 	}
 
 	onMount(async () => {
 		const data = await getLayeredSvgFromBannyIndex(1);
-        let tokens: HTMLCollectionOf<Element> | Element[] = document.getElementsByClassName('bannyPlaceholder')
-        tokens = Array.from(tokens);
-        tokens.forEach(token => {
-            token.innerHTML = data.image;
-        })
-        jbxRange = data.jbx_range;
+		let tokens: HTMLCollectionOf<Element> | Element[] =
+			document.getElementsByClassName('bannyPlaceholder');
+		tokens = Array.from(tokens);
+		tokens.forEach((token) => {
+			token.innerHTML = data.image;
+		});
+		jbxRange = data.jbx_range;
 	});
 </script>
 
 <section>
 	{#each lockPeriods as lockPeriod, i}
-		<Background topText={lockPeriod} bottomText={`${jbxRange} JBX`} options={getOptionsForLockPeriod(i)}>
-            <g class="bannyPlaceholder" />
-        </Background>
+		<Background
+			topText={lockPeriod}
+			bottomText={`${jbxRange || '...loading'} JBX`}
+			options={getOptionsForLockPeriod(i)}
+		>
+			<g class="bannyPlaceholder" />
+		</Background>
 	{/each}
 </section>
 
