34ffb905fe4195757a6029472ba7a2b5115e0fb4
tankbottoms <tanktops@tankbottoms.xyz>
2022-06-23 17:25:03 -0400

    Added StringHelpers, and Todo notes

--- solidity/test/types.ts
+++ solidity/test/types.ts
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
 export enum AssetDataType {
-    AUDIO_MP3,
-    IMAGE_SVG,
-    IMAGE_PNG
+  AUDIO_MP3,
+  IMAGE_SVG,
+  IMAGE_PNG,
 }
 
 export enum AssetAttrType {
-    STRING_VALUE,
-    BOOLEAN_VALUE,
-    UINT_VALUE,
-    INT_VALUE,
-    TIMESTAMP_VALUE
+  STRING_VALUE,
+  BOOLEAN_VALUE,
+  UINT_VALUE,
+  INT_VALUE,
+  TIMESTAMP_VALUE,
 }
